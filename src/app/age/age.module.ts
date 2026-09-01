f13a04fc4d1b44811ad96efa85897a73118f048b
tomasb <tomas.baltrukonis@gmail.com>
2026-09-01 20:09:43 +0300

    age component cleanup

--- src/app/age/age.module.ts
+++ src/app/age/age.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AgeComponent } from './age/age.component';
 import { MatInputModule } from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from '@angular/material/button';
-import {MatGridListModule} from "@angular/material/grid-list";
 
 
 @NgModule({
@@ -16,10 +14,8 @@ import {MatGridListModule} from "@angular/material/grid-list";
     CommonModule,
     MatInputModule,
     FormsModule,
-    MatIconModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatGridListModule
+    MatButtonModule
   ]
 })
 export class AgeModule { }
--- src/app/age/age.service.ts
+++ src/app/age/age.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import { Observable } from "rxjs";
-import { InvoicePaymentSummary } from "../models/invoice-payment-summary";
 import {AgeCategoryResult} from "../models/age-category-result";
 
 @Injectable({
@@ -11,8 +10,6 @@ import {AgeCategoryResult} from "../models/age-category-result";
 export class AgeService {
 
   private apiUrl = environment.apiUrl + '/api/age-category';
-
-  private ageDefaultValue: number = 0;
 
   constructor(private http: HttpClient) { }
 
